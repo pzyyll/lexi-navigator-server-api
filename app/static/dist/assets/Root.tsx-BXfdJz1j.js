fdf697504d86d8eda05e89042f68883ef1d31290
ZHILICAI-mbp <pzyyll@gmail.com>
2024-03-22 23:52:03 +0800

    push

---
+++ app/static/dist/assets/Root.tsx-BXfdJz1j.js
@@ -0,0 +1,13 @@
+import{k as t,s as o,I as s,a as e,j as n,C as i}from"./vendor-BdbgMDah.js";import{S as r}from"./Dashboard.tsx-By4d6ZQ0.js";const m=t`
+  0%, 100% { background-color: #f8f8f8; }
+  50% { background-color: #f0f0f0; }
+`,f=t`
+  0% { transform: scale(1); }
+  14% { transform: scale(1.4); }
+  28% { transform: scale(1); }
+  42% { transform: scale(1.4); }
+  70% { transform: scale(1); }
+`,c=o(s)(({theme:a})=>({animation:`${m} 3s ease-in-out infinite, ${f} 3s ease-in-out infinite`})),l=t`
+  0% { transform: rotateY(0deg); }
+  100% { transform: rotateY(360deg); }
+`,x=o(e)(({theme:a})=>({animation:`${l} 5s linear infinite`}));function g(){return n.jsxs(i,{component:"main",maxWidth:"sm",children:[n.jsx(c,{children:n.jsx(r,{viewBox:"0 0 1091.6 1091.6"})}),n.jsx(x,{variant:"h4",sx:{mt:"20px",fontWeight:"bold"},children:"Lexi Navigator"})]})}export{g as R};
