fa71df333293ca606a5ec44589a9c289aed6f82a
ZHILICAI-mbp <pzyyll@gmail.com>
2024-03-23 02:59:56 +0800

    yes

---
+++ app/common/speech_api/gspeech.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# @Date: "2024-03-23"
+# @Description: google speech api
+
+from google.cloud import texttospeech
+
+
+class GSpeechAsyncClient:
+
+    def init(self, conf=None):
+        self.conf = conf.copy if conf else {}
+        self.client = texttospeech.TextToSpeechAsyncClient()
+
+    async def text_to_speech(self, text, language_code):
+        synthesis_input = texttospeech.SynthesisInput(text=text)
+        voice = texttospeech.VoiceSelectionParams(
+            language_code=language_code,
+            ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL,
+        )
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3)
+        response = await self.client.synthesize_speech(input=synthesis_input,
+                                                       voice=voice,
+                                                       audio_config=audio_config)
+        return response.audio_content
--- app/routers/api/translate/__init__.py
+++ app/routers/api/translate/__init__.py
@@ -3,3 +3,4 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/translate")
 
 from . import text
+from . import speech
