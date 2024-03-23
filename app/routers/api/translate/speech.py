2a8c601b6c13432266189fcf4a4470c93099ffe2
ZHILICAI-mbp <pzyyll@gmail.com>
2024-03-23 17:59:49 +0800

    add

--- app/routers/api/translate/speech.py
+++ app/routers/api/translate/speech.py
@@ -20,16 +20,24 @@ temp_dir = pathlib.Path(settings.static_path, relative_temp).resolve()
 
 async def get_mp3_response(text, language_code, user: UserInDB):
     audio_content = await gspeech.text_to_speech(text, language_code)
-    file_name = f"{user.userid}.mp3"
+    # file_name = f"{user.userid}.mp3"
 
-    def _write_file():
-        temp_dir.mkdir(parents=True, exist_ok=True)
-        file = temp_dir.joinpath(file_name)
-        file.write_bytes(audio_content)
+    # def _write_file():
+    #     temp_dir.mkdir(parents=True, exist_ok=True)
+    #     file = temp_dir.joinpath(file_name)
+    #     file.write_bytes(audio_content)
 
-    await asyncio.get_event_loop().run_in_executor(None, _write_file)
+    # await asyncio.get_event_loop().run_in_executor(None, _write_file)
 
-    return {"url_relative": f"{relative_temp}/{file_name}"}
+    # return {"url_relative": f"{relative_temp}/{file_name}"}
+    streamfile = await asyncio.get_event_loop().run_in_executor(
+        None, io.BytesIO, audio_content
+    )
+    return StreamingResponse(
+        streamfile,
+        media_type="audio/mpeg",
+        headers={"Content-Disposition": "attachment; filename=text-to-speech.mp3"},
+    )
 
 
 @router.get("/text-to-speech")
