3240f1368fcfc03995b38fb56658375510134d41
ZHILICAI-mbp <pzyyll@gmail.com>
2024-03-23 17:17:17 +0800

    up

--- app/routers/api/translate/speech.py
+++ app/routers/api/translate/speech.py
@@ -20,13 +20,12 @@ temp_dir = pathlib.Path(settings.static_path, relative_temp).resolve()
 
 async def get_mp3_response(text, language_code, user: UserInDB):
     audio_content = await gspeech.text_to_speech(text, language_code)
-    file_name = f"{user.userid}{language_code}{urlencode(text)}.mp3"
+    file_name = f"{user.userid}.mp3"
 
     def _write_file():
         temp_dir.mkdir(parents=True, exist_ok=True)
         file = temp_dir.joinpath(file_name)
-        if not file.exists():
-            file.write_bytes(audio_content)
+        file.write_bytes(audio_content)
 
     await asyncio.get_event_loop().run_in_executor(None, _write_file)
 
